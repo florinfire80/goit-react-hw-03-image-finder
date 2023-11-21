1238b2ff9c4ccb8589deab917ec36609652d3adc
Florin <florinfire80@yahoo.com>
2023-11-21 09:55:40 +0200

    modificari cod

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,29 +1,14 @@
 // ImageGalleryItem.jsx
 
-import React, { useState } from 'react';
-import basicLightbox from 'basiclightbox';
+import React, { useState, useEffect } from 'react';
 import CustomModal from '../Modal/Modal';
 import styles from './ImageGalleryItem.module.css';
 
 const ImageGalleryItem = ({ webformatURL, largeImageURL, altText }) => {
-  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const handleImageClick = () => {
-    const instance = basicLightbox.create(`
-      <div class="overlay">
-        <div class="modal">
-          <img src="${largeImageURL}" alt="${altText}" />
-        </div>
-      </div>
-    `);
-
-    instance.show();
-
-    setModalIsOpen(true);
-  };
-
-  const handleCloseModal = () => {
-    setModalIsOpen(false);
+    setIsModalOpen(true);
   };
 
   return (
@@ -36,8 +21,8 @@ const ImageGalleryItem = ({ webformatURL, largeImageURL, altText }) => {
       />
       {
         <CustomModal
-          isOpen={modalIsOpen}
-          onRequestClose={handleCloseModal}
+          isOpen={isModalOpen}
+          onRequestClose={() => setIsModalOpen(false)}
           webformatURL={webformatURL}
           largeImageURL={largeImageURL}
           altText={altText}
@@ -48,3 +33,53 @@ const ImageGalleryItem = ({ webformatURL, largeImageURL, altText }) => {
 };
 
 export default ImageGalleryItem;
+
+// ===========================
+// import React, { useState } from 'react';
+// import basicLightbox from 'basiclightbox';
+// import CustomModal from '../Modal/Modal';
+// import styles from './ImageGalleryItem.module.css';
+
+// const ImageGalleryItem = ({ webformatURL, largeImageURL, altText }) => {
+//   const [modalIsOpen, setModalIsOpen] = useState(false);
+
+//   const handleImageClick = () => {
+//     const instance = basicLightbox.create(`
+//       <div class="overlay">
+//         <div class="modal">
+//           <img src="${largeImageURL}" alt="${altText}" />
+//         </div>
+//       </div>
+//     `);
+
+//     instance.show();
+
+//     setModalIsOpen(true);
+//   };
+
+//   const handleCloseModal = () => {
+//     setModalIsOpen(false);
+//   };
+
+//   return (
+//     <li className={styles['gallery-item']}>
+//       <img
+//         className={styles['gallery-item-image']}
+//         src={webformatURL}
+//         alt={altText}
+//         onClick={handleImageClick}
+//       />
+//       {
+//         <CustomModal
+//           isOpen={modalIsOpen}
+//           onRequestClose={handleCloseModal}
+//           webformatURL={webformatURL}
+//           largeImageURL={largeImageURL}
+//           altText={altText}
+//         />
+//       }
+//     </li>
+//   );
+// };
+
+// export default ImageGalleryItem;
