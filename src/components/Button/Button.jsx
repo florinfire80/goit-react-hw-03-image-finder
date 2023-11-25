ed38b3b56909f36bd63623997cbc8797fad10729
Florin <florinfire80@yahoo.com>
2023-11-25 16:05:13 +0200

    modify

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -18,21 +18,3 @@ class Button extends Component {
 }
 
 export default Button;
-
-// ===============================
-// import React from 'react';
-// import styles from './Button.module.css';
-
-// const Button = ({ onClick, isVisible }) => {
-//   return (
-//     <button
-//       className={styles['button']}
-//       onClick={onClick}
-//       style={{ display: isVisible ? 'block' : 'none' }}
-//     >
-//       Load more
-//     </button>
-//   );
-// };
-
-// export default Button;
--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -13,17 +13,3 @@ class Loader extends Component {
 }
 
 export default Loader;
-
-// import React from 'react';
-// import { CirclesWithBar } from 'react-loader-spinner';
-
-// const Loader = () => {
-//   return (
-//     <div style={{ textAlign: 'center', marginTop: '20px' }}>
-//       <CirclesWithBar type="Oval" color="#00BFFF" height={50} width={50} />
-//       <p>Loading...</p>
-//     </div>
-//   );
-// };
-
-// export default Loader;
